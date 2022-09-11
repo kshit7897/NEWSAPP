d1edcbd654f29ef85963417532a4cb735ebd19f4
kshit <chauhanparikshit7@gmail.com>
2022-09-11 16:22:36 +0530

    api working

--- news.js
+++ news.js
@@ -1,16 +1,23 @@
 
 let btn = document.querySelector('.search')
-let input = document.querySelector('.input')
+let input = document.querySelector('.newsinput')
 let headline = document.querySelector('.headline')
 let para = document.querySelector('.para')
 
 btn.addEventListener("click", function () {
+    // GET https://newsapi.org/v2/top-headlines?country=us&apiKey=API_KEY
+
     // https://newsapi.org/v2/top-headlines?country=us&apiKey=7689a33b160242fe868be519e87f7035
-    fetch("https://newsapi.org/v2/top-headlines?country="+ input.value 
-    +"7689a33b160242fe868be519e87f7035")
+    fetch(`https://newsapi.org/v2/everything?q=${input.value}&from=2022-08-11&sortBy=publishedAt&apiKey=7689a33b160242fe868be519e87f7035`)
 
     .then(result => result.json())
     .then(data => {
-        console.log(data)
+        console.log(data);
+
+        var head1 = data [articles][0][title]
+
+
+
+        headline.textContent = head1
     })
 })
